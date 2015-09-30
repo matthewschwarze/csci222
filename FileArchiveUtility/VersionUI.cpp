ee46d890c83839784fa6a84302730be818e6b5ec
matthew <matthew.schwarze@gmail.com>
2015-09-30 19:10:01 +1000

    finished adding comments

--- FileArchiveUtility/VersionUI.cpp
+++ FileArchiveUtility/VersionUI.cpp
@@ -20,42 +20,12 @@ VersionUI::VersionUI() {
         std::cout << "connected ok" << std::endl;
         std::string tmp = "fileArchiver.cpp";
         std::string comment = "zipped test ";
-        if (db->exists(tmp)) {
-            cout << "already exists" << std::endl;
-            //call differs
-            if (db->differs(tmp)) {
-                cout << "they are different" << endl;
-                 db->update(tmp, comment);
-            } else {
-                std::cout << "they are not different" << endl;
-            }
-        } else {
-            db->insertNew(tmp, comment);
-        }
-        tmp = "fileArchiver.cpp";
-        comment = "~/";
-        //cout << "getting version 0" << endl;
-       // db->retriveVersion(0, tmp, comment);
-        //for (int i = 0; i < 4; i++)
-           // db.removeVersion(i, tmp);
         vector<VersionRec>* vers= new vector<VersionRec>;
         (*vers) = db->getVersioninfo(tmp);
-        //cout << "versions info" << endl;*/
+        
         
         //make the table to display
         createTable(vers);
-        /*for(vector<VersionRec>::iterator it = (*vers).begin(); it != (*vers).end(); ++it)
-            cout << (*it).getVersionNumber() << " " << (*it).getLength() << " " << (*it).getModifyTime().tv_sec << endl;
-        
-        
-        int curr = db->getCurrentVersionNumber(tmp);
-        cout << "current version " <<curr << endl;
-        
-         string comm = db->getComment(tmp, curr);
-        cout << "comment of version " << curr << " " <<comm << endl;
-   
-        string hash = db->getHashOfLastSaved(tmp);
-        cout << "current hash " <<hash << endl;*/
         
         //when the buttons are pressed, open their respective UIs and functions
         connect(widget.browseButton, SIGNAL(clicked()), this, SLOT(openFile()));    //open up the native browser to select a file
